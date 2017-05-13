f726debd8d5390619321762a8c5c7ce0e1f0ed75
gonzalovazquez <gonzalovazquez010@gmail.com>
2017-05-13 15:05:30 -0400

    Chore: Removed redirect

--- src/index.jsx
+++ src/index.jsx
@@ -4,21 +4,8 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 
-// Vendor libraries
-import hello from './vendor/hello.all.js';
-
 // Components:
 import App from './App';
-
-
-// Initalize hello.js
-// https://adodson.com/hello.js/
-// Passing clientID and scope permissions
-hello.init({
-  facebook: 'FACEBOOK_CLIENT_ID',
-  windows: 'WINDOWS_CLIENT_ID',
-  google: 'GOOGLE_CLIENT_ID'
-}, {redirect_uri: 'redirect.html'});
 
 const Root = () => (
   <MuiThemeProvider>
