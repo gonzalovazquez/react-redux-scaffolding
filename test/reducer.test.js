d6de95a10ab922d7836ae66ec4e4f24b9cb6c5da
gonzalovazquez <gonzalovazquez010@gmail.com>
2017-05-19 20:07:23 -0400

    Feat: Added test to reducer

---
+++ test/reducer.test.js
@@ -0,0 +1,61 @@
+import chai from 'chai';
+import todo from '../src/reducers/todo';
+
+const expect = chai.expect;
+
+const initialState = {
+  todos: [],
+};
+
+const endState = {
+  todos: [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    }
+  ]
+};
+
+describe('todo reducer', () => {
+
+  it('should return the initial state', () => {
+    expect(todo({}, {})).to.deep.equal({});
+  });
+
+  it('should return loading state', () => {
+    expect(todo({}, { 
+      type: 'TODO_IS_LOADING', 
+      isLoading: true })
+    ).to.deep.equal({ isLoading: true });
+  });
+
+  it('should return error state', () => {
+    expect(todo({}, { 
+      type: 'TODO_HAS_ERROR', 
+      hasErrored: true })
+    ).to.deep.equal({ hasErrored: true });
+  });
+
+  it('should return loading state', () => {
+    expect(todo({}, { 
+      type: 'TODO_IS_LOADING', 
+      isLoading: true })
+    ).to.deep.equal({ isLoading: true });
+  });
+
+  it.only('should return successful state', () => {
+    expect(todo(initialState, { 
+      type: 'TODO_IS_SUCCESSFUL', 
+      items: endState.todos })
+    ).to.deep.equal(endState);
+  });
+
+});
